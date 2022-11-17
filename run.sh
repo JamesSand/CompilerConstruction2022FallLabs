ce02ffc299604675640078fc4eb5d03a977a35de
JamesSand <shazz20@mails.tsinghua.edu.cn>
2022-11-17 18:37:54 +0800

    check back to parser

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
-python3.9 main.py --tac --input minidecaf-tests/failcases/step9/var_shadows_param.c
+python3.9 main.py --parse --input minidecaf-tests/testcases/step9/arg_reg_0.c
 # python3.9 main.py --tac --input minidecaf-tests/testcases/step9/fib.c
